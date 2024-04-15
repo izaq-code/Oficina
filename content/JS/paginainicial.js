6c1df46c0ba3243f80e5d13ad4471b1c6f65743c
iannovais <iandosreisnovais@gmail.com>
2024-04-15 14:24:16 -0300

    Atualizado css da página inicial

--- content/JS/paginainicial.js
+++ content/JS/paginainicial.js
@@ -1,10 +1,11 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+    let icon = document.querySelector('.icon');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_black_36dp.svg";
+        icon.src = "content/img/menu_black_36dp.svg"; // Volta para a imagem do menu
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_black_36dp.svg";
+        icon.src = "content/img/close_black_36dp.svg"; // Muda para a imagem de fechar
     }
 }
