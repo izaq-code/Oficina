c967374c66d3bc7c629e294f6c8fffdf335d0fe0
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-04-17 16:10:24 -0300

    Update exibir.js

--- content/JS/exibir.js
+++ content/JS/exibir.js
@@ -1,53 +1,55 @@
 document.addEventListener("DOMContentLoaded", exibir);
 window.addEventListener("load", exibir);
-function exibir(){
-    $(document).ready(function(){
+function exibir() {
+    $(document).ready(function () {
         $.ajax({
             type: 'POST',
             url: '../PHP/exibir.php',
             contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
             dataType: 'json',
-            success: function(data){
+            success: function (data) {
                 exibir(data);
             }
         });
     });
 }
 
-function exibir(data){
-    
-   console.log(data);
+function exibir(data) {
 
-   q = $('#informacao-aberta');
-   q.empty();
+    console.log(data);
+    var solicitacao = data.solicitacao;
+    var exibicao = data.exibicao;
 
-   q.append(solicitacao[0].aberta);
+    q = $('#informacao-aberta');
+    q.empty();
 
-   w = $('#informacao-concluida');
-   w.empty();
+    q.append(solicitacao[0].aberta);
 
-   w.append(solicitacao[0].finalizada);
+    w = $('#informacao-concluida');
+    w.empty();
 
-   
-   t = $('#mostrar');
-   t.empty();
-   
-   var i = 1;
-   
-   data.forEach(function(e){
-      
-      let adicionar = (
-      "<div class='status'>" 
-      + "<h1> Solicitação " + i + "</h1><br>" 
-      + "<h6 class='negro'> Modelo: </h6> <p>" + e['modelo'] + "</p><br>" 
-      + "<h6 class='negro'> Sinistro: </6> <p>" + e['sinistro'] + "</p> <br><br>"
-      + "<h6 class='negro'> Status: </h6> <p>" + e['status_veiculo'] + "</p>"
-      +"</div>"
-      )
-      
-      i++;
-      
-      t.append(adicionar);
-   });
-   
+    w.append(solicitacao[0].finalizada);
+
+
+    t = $('#mostrar');
+    t.empty();
+
+    var i = 1;
+
+    exibicao.forEach(function (e) {
+
+        let adicionar = (
+            "<div class='status'>"
+            + "<h1> Solicitação " + i + "</h1><br>"
+            + "<h6 class='negro'> Modelo: </h6> <p>" + e['modelo'] + "</p><br>"
+            + "<h6 class='negro'> Sinistro: </6> <p>" + e['sinistro'] + "</p> <br><br>"
+            + "<h6 class='negro'> Status: </h6> <p>" + e['status_veiculo'] + "</p>"
+            + "</div>"
+        )
+
+        i++;
+
+        t.append(adicionar);
+    });
+
 }
