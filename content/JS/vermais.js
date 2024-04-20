79789a13d6a76a7694ea2b5c54399988cff1f0f6
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-04-20 00:55:25 -0300

    Add files via upload

---
+++ content/JS/vermais.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", exibirDados);
+
+function exibirDados () {
+    var cod_veiculo = localStorage.getItem('codVeiculo');
+    
+    $.ajax({
+        type: 'POST',
+        url: '../PHP/vermais.php',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: 'json',
+        data: {
+            cod_veiculo: cod_veiculo
+        },
+        success: function (data) {
+            exibir(data);
+        }
+    });
+}
+
+function exibir (data) {
+
+    carro = data.carro;
+
+
+     w =  $('#exibirmais');
+     w.empty();
+
+     var divContent = "<div class='exibirtudo'>";
+     for (var key in data[0]) {
+         if (!isNaN(key)) continue; // Ignorar chaves numéricas
+         divContent += "<h3>" + key.replace('_', ' ') + ":</h3>" + "<p>" + data[0][key] + "</p>";
+     }
+     divContent += "</div>";
+ 
+     w.append(divContent);
+}
