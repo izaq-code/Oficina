5f1d12eb8fd5290c278aff6696b531a09d5f49f9
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-04-26 16:40:53 -0300

    navbar

--- content/JS/navbar.js
+++ content/JS/navbar.js
@@ -1,36 +1,8 @@
+document.getElementById("bnte").addEventListener("click", function() {
+    document.querySelector(".nav").classList.toggle("active");
+});
 
-document.addEventListener("DOMContentLoaded", function () {
-    var button = document.getElementById("bnte");
-    var header = document.getElementById("header");
-    var nav = document.querySelector(".nav");
-  
-    button.addEventListener("click", toggleHeaderStyle);
-  
-    function toggleHeaderStyle() {
-        var isCollapsed = header.classList.contains("collapsed");
-  
-        if (isCollapsed) {
-            header.classList.remove("collapsed");
-            nav.style.display = "block";
-        } else {
-            header.classList.add("collapsed");
-            nav.style.display = "none";
-        }
-    }
-  
-    button.addEventListener("click", toggleHeaderStyle2);
-  
-    function toggleHeaderStyle2() {
-        var header = document.getElementById("header");
-  
-        if (header.classList.contains("hd")) {
-            header.classList.remove("hd");
-            header.classList.add("hd1");
-        } else {
-            header.classList.remove("hd1");
-            header.classList.add("hd");
-        }
-    }
-  });
-  
-  
+
+document.getElementById("bnte").addEventListener("click", function() {
+    document.querySelector("#bnte").classList.toggle("active");
+});
