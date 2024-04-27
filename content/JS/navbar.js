8cef8391e0abef3fd626cf14d750cef3753a0662
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-04-26 23:37:05 -0300

    js

--- content/JS/navbar.js
+++ content/JS/navbar.js
@@ -1,8 +1,34 @@
-document.getElementById("bnte").addEventListener("click", function() {
-    document.querySelector(".nav").classList.toggle("active");
-});
 
 
-document.getElementById("bnte").addEventListener("click", function() {
-    document.querySelector("#bnte").classList.toggle("active");
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("bnte").addEventListener("click", function() {
+        document.querySelector("#bnte").classList.toggle("active");
+    });
+    
+
+    const nav = document.querySelector('.nav');
+    const toggleButton = document.getElementById('bnte');
+    const sairDiv = document.querySelector('#sair');
+
+    const homeLink = document.querySelector('#home a');
+    const listaLink = document.querySelector('#lista a');
+
+
+        
+    toggleButton.addEventListener('click', function() {
+        nav.classList.toggle('active');
+
+
+        
+        if (nav.classList.contains('active')) {
+            homeLink.style.display = 'inline-block';
+            listaLink.style.display = 'inline-block';
+            sairDiv.style.display = 'inline-block';
+        } else {
+            homeLink.style.display = 'none';
+            listaLink.style.display = 'none';
+            sairDiv.style.display = 'none';
+        }
+    });
 });
