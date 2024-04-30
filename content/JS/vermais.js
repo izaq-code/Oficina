f54afd1dbbd5046bafb0771ce1b5c29688f6c24f
Porttuga <161348581+Porttuga@users.noreply.github.com>
2024-04-30 13:16:43 -0300

    Atualizado com download PDF e as Imagens

--- content/JS/vermais.js
+++ content/JS/vermais.js
@@ -23,7 +23,6 @@ function exibir(data) {
     carro = data.carro;
     resp = data.sim;
 
-
     w = $('#exibirmais');
     w.empty();
 
@@ -36,19 +35,82 @@ function exibir(data) {
     }
 
     if (resp === true) {
-        ww = $('#exibirmaiss');
+        var ww = $('#exibirmaiss');
         ww.empty();
-        fotos = data.fotos;
+        var fotos = data.fotos;
+        var pdf = data.pdf;
+        var pdfCaminho = '../upload/' + pdf;
 
         for (var key in fotos[0]) {
             if (!isNaN(key)) continue; // Ignorar chaves numéricas
             var divContent = "<div class='exibirtuudo'>";
-            divContent +=  "<div class='img-conteiner'><img  src = '"+ fotos[0][key] +"'class='imgg'></div> <h3>" + key.replace('_', ' ') + ":</h3><br>" ;
+            divContent += "<div class='img-container'><img src='" + fotos[0][key] + "' class='imgg'></div><h3>" + key.replace('_', ' ') + ":</h3>";
+            divContent += "<input type='checkbox' id='imagens' class='img-checkbox' data-caminho='" + fotos[0][key] + "'>";
             divContent += "</div>";
             ww.append(divContent);
         }
+         
+        if (pdf) {
+             pdf= "<iframe  src='" + pdf + "'></iframe>";
+            ww.append(pdf); 
+        }   
 
+        var download = "<button id='download-pdf'> Download-PDF </button>";
+        ww.append(download);
 
-    }
+        var download = "<button id='bnt-baixar'> Download </button>";
+        ww.append(download);
 
+        $("#download-pdf").click(function() {
+            var link = document.createElement('a');
+            link.href = pdfCaminho;
+            link.download = 'Documento.pdf';
+            link.target = '_self';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+
+        $("#bnt-baixar").click(function() {
+            var imagens_selecionadas = [];
+            
+            $(".img-checkbox:checked").each(function () {
+                imagens_selecionadas.push($(this).data('caminho'));
+            });
+        
+            if (imagens_selecionadas.length === 0) {
+                alert('Nenhuma imagem selecionada para download');
+                return;
+            }
+        
+            var zip = new JSZip();
+            var diretorioImagens = '../upload/';
+        
+            var promises = []; 
+        
+            imagens_selecionadas.forEach(function(caminhoImagem) {
+                var nomeArquivo = caminhoImagem.substring(caminhoImagem.lastIndexOf('/') + 1);
+                var caminhoCompleto = diretorioImagens + nomeArquivo;
+        
+                var promise = fetch(caminhoCompleto)
+                    .then(response => response.blob())
+                    .then(blob => {
+                        zip.file(nomeArquivo, blob, { binary: true });
+                    });
+        
+                promises.push(promise);
+            });
+        
+            Promise.all(promises).then(function() {
+                zip.generateAsync({ type: 'blob' }).then(function(content) {
+                    var zipNome = 'Imagens.zip';
+                    var link = document.createElement('a');
+                    link.download = zipNome;
+                    link.href = URL.createObjectURL(content);
+                    link.click();
+                });
+            });
+        });
+        
+  }
 }
