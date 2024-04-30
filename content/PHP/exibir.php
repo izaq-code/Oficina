cb5d10e8072092a13bbc1c04bd2fa86a85fa4271
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-04-30 14:57:36 -0300

    Update exibir.php

--- content/PHP/exibir.php
+++ content/PHP/exibir.php
@@ -1,38 +1,42 @@
 <?php
-include_once('conexao.php');
+
+session_start();
+include_once ('conexao.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $sql = mysqli_query ($conexao,"SELECT cod_veiculo, id_personalizado, modelo, sinistro, status_veiculo 
-                                    FROM carro
+    $id = $_SESSION['id_usuario'];
+
+    $sql = mysqli_query($conexao, "SELECT cod_veiculo, id_personalizado, modelo, sinistro, status_veiculo 
+                                    FROM carro WHERE cod_seguradora = '$id';
 ");
 
-    $informacoes = mysqli_query ($conexao, "SELECT SUM(CASE WHEN  status_veiculo = 'aberto' THEN 1 ELSE 0 END),
+    $informacoes = mysqli_query($conexao, "SELECT SUM(CASE WHEN  status_veiculo = 'aberto' THEN 1 ELSE 0 END),
                                             SUM(CASE WHEN status_veiculo = 'Finalizado' THEN 1 ELSE 0 END),
                                             SUM(CASE WHEN status_veiculo = 'Recusado' THEN 1 ELSE 0 END),
                                             SUM(CASE WHEN status_veiculo = 'Aceito' THEN 1 ELSE 0 END)
-                                            FROM carro;    
+                                            FROM carro WHERE cod_seguradora = '$id';    
 ");
 
-$solicitacao = array();
+    $solicitacao = array();
 
 
-while($row = mysqli_fetch_array($informacoes)) {
+    while ($row = mysqli_fetch_array($informacoes)) {
         $solicitacao[] = $row;
     }
-    
-$exibicao = array();
-    
-while($row = mysqli_fetch_array($sql)) {
+
+    $exibicao = array();
+
+    while ($row = mysqli_fetch_array($sql)) {
         $exibicao[] = $row;
     }
 
-$tiago = array(
-    'solicitacao' => $solicitacao,
-    'exibicao' => $exibicao
-);
+    $tiago = array(
+        'solicitacao' => $solicitacao,
+        'exibicao' => $exibicao
+    );
 
- echo json_encode($tiago);
+    echo json_encode($tiago);
 
 }
 
