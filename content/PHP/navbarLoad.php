3639944bead31706a2f72e93372aa3d873d85179
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-05-01 15:40:17 -0300

    Update navbarLoad.php

--- content/PHP/navbarLoad.php
+++ content/PHP/navbarLoad.php
@@ -7,12 +7,27 @@ include_once ("conexao.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $cod = $_SESSION['id_usuario'];
+    $isOf = $_SESSION['isOf'];
+    $query;
 
-    $slq = mysqli_query($conexao, "SELECT ra.nome_funcionario nome_fulano, ra.ra email_fulano from login_adm
-    INNER join ra on login_adm.cod_ra = ra.cod_ra WHERE login_adm.cod_adm = '$cod'");
+    if ($isOf) {
 
+        $query = "SELECT 
+                    ra.nome_funcionario nome_fulano, 
+                    ra.ra email_fulano  from login_adm
+                    INNER join ra on 
+                    login_adm.cod_ra = ra.cod_ra 
+                    WHERE login_adm.cod_adm = '$cod'";
+    } else {
+        $query = "SELECT 
+                    nome nome_fulano, email email_fulano  
+                    FROM login_seguradora 
+                    WHERE cod_seguradora = '$cod'";
+    }
 
-    while($row = mysqli_fetch_array($slq)){
+    $sql = mysqli_query($conexao, $query);
+
+    while ($row = mysqli_fetch_array($sql)) {
 
         $sim = $row;
 
