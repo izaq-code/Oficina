0227ee1773e4177f3a79d68ac1d82525ba382975
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-05-01 22:22:40 -0300

    Update navbarLoad.php

--- content/PHP/navbarLoad.php
+++ content/PHP/navbarLoad.php
@@ -14,13 +14,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $query = "SELECT 
                     ra.nome_funcionario nome_fulano, 
-                    ra.ra email_fulano  from login_adm
+                    ra.ra email_fulano,
+                    Login_adm.foto foto_perfil  
+                    from login_adm
                     INNER join ra on 
                     login_adm.cod_ra = ra.cod_ra 
                     WHERE login_adm.cod_adm = '$cod'";
     } else {
         $query = "SELECT 
-                    nome nome_fulano, email email_fulano  
+                    nome nome_fulano, email email_fulano, foto foto_perfil  
                     FROM login_seguradora 
                     WHERE cod_seguradora = '$cod'";
     }
