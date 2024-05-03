e89861f075e94fa8824bccb0499d9a9030281913
cout"izaq <122700689+izaq-code@users.noreply.github.com>
2024-05-03 19:39:07 -0300

    navbar

--- content/JS/navbar.js
+++ content/JS/navbar.js
@@ -1,12 +1,8 @@
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("bnte").addEventListener("click", function() {
-        document.querySelector("#bnte").classList.toggle("active");
-        document.querySelector("#bb").classList.toggle("active");
-    });
+
     const nav = document.querySelector('.nav');
     const toggleButton = document.getElementById('bnte');
+    const iconee = document.querySelector('#iconee');
     const homeLink = document.querySelector('#home a');
     const listaLink = document.querySelector('#lista a');
     const emailFulano = document.querySelector('#email-fulano');
@@ -14,18 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     toggleButton.addEventListener('click', function() {
         nav.classList.toggle('active');
+        toggleButton.classList.toggle('active'); 
+        document.querySelector("#bb").classList.toggle("active");
 
         if (nav.classList.contains('active')) {
             homeLink.style.display = 'inline-block';
             listaLink.style.display = 'inline-block';
-            emailFulano.style.display = 'block'; // Mostrar email-fulano
-            nomeFulano.style.display = 'block'; // Mostrar nome-fulano
+            emailFulano.style.display = 'block'; 
+            nomeFulano.style.display = 'block'; 
+            iconee.classList.add('active'); 
         } else {
             homeLink.style.display = 'none';
             listaLink.style.display = 'none';
-            emailFulano.style.display = 'none'; // Ocultar email-fulano
-            nomeFulano.style.display = 'none'; // Ocultar nome-fulano
+            emailFulano.style.display = 'none'; 
+            nomeFulano.style.display = 'none'; 
+            iconee.classList.remove('active'); 
         }
     });
 });
-
