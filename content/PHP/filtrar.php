344ee7e1fc72eac56affc2f0ca9b10722deb85c9
Porttuga <161348581+Porttuga@users.noreply.github.com>
2024-05-03 12:56:30 -0300

    Contem  a opção para mostrar todos os status

--- content/PHP/filtrar.php
+++ content/PHP/filtrar.php
@@ -1,10 +1,21 @@
 <?php
 
-include_once('conexao.php');
-
-$selecionado = $_POST['selecionado'];
-
+include_once ('conexao.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $selecionado = $_POST['selecionado'];
+
+    if ($selecionado === 'Padrao') {
+    $todos = mysqli_query($conexao, "SELECT cod_veiculo, id_personalizado, modelo, sinistro, status_veiculo 
+                                     FROM carro
+");
+
+while ($row = (mysqli_fetch_array($todos))) {
+    $todos_status[] = $row;
+}
+
+echo json_encode($todos_status,  JSON_UNESCAPED_SLASHES);
+}else{
 
     $sql = mysqli_query($conexao, "SELECT cod_veiculo, id_personalizado, modelo, sinistro, status_veiculo 
                                     FROM carro
@@ -12,11 +23,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ");
 
     $status = array();
+    $todos_status = array();
 
-    while($row = (mysqli_fetch_array($sql))){
+    while ($row = (mysqli_fetch_array($sql))) {
         $status[] = $row;
     }
 
-    echo json_encode($status, JSON_UNESCAPED_SLASHES);
+    echo json_encode($status,   JSON_UNESCAPED_SLASHES);
+}
 }
 ?>
