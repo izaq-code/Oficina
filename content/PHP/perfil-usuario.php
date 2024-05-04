680a48f80c696344db10897b552844f6180a796a
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-05-04 12:20:15 -0300

    Update perfil-usuario.php

--- content/PHP/perfil-usuario.php
+++ content/PHP/perfil-usuario.php
@@ -6,8 +6,18 @@ include_once ('conexao.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $caminhoArquivoPDF;
+    $fotoAntiga;
+    $semFoto = '../fotos-usuarios/semfoto.png';
     $cod = $_SESSION['id_usuario'];
     $isOf = $_SESSION['isOf'];
+    $apagou = $_POST['apagou'];
+
+    $consulta = mysqli_query($conexao, "SELECT foto from login_seguradora where cod_seguradora = '$cod'");
+
+    while ($row = mysqli_fetch_array($consulta)) {
+        $fotoAntiga = $row['foto'];
+    }
+
 
     if (isset($_FILES['foto__input9'])) {
 
@@ -17,6 +27,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (move_uploaded_file($_FILES['foto__input9']['tmp_name'], $pdfNomeArquivo)) {
             $caminhoArquivoPDF = mysqli_real_escape_string($conexao, $pdfNomeArquivo);
         }
+
+    }
+
+    if (!isset($_FILES['foto__input9'])) {
+        if (!$apagou) {
+            $caminhoArquivoPDF = $fotoAntiga;
+        }
+        if ($apagou) {
+            $caminhoArquivoPDF = $semFoto;
+        }
+    }
+
+    if ($isOf == true) {
+
+        $sql = "UPDATE login_adm
+        SET foto = '$caminhoArquivoPDF'
+        WHERE cod_adm = '$cod';
+
+    ";
+
+        if ($conexao->query($sql) === TRUE) {
+            echo "Inserção realizada com sucesso";
+        } else {
+            echo "Erro na inserção: " . $conexao->error;
+        }
+        
+        exit();
     }
 
     $nome = $_POST['nome'];
