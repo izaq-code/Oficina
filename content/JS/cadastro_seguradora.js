967239fead56f8729a8a9e660d19ba2322428f1d
Porttuga <161348581+Porttuga@users.noreply.github.com>
2024-05-05 22:54:18 -0300

    Com validador de CNPJ

--- content/JS/cadastro_seguradora.js
+++ content/JS/cadastro_seguradora.js
@@ -3,6 +3,53 @@ function hashSenha(senha) {
     return hash;
 }
 
+function validarCNPJ(cnpj) {
+  cnpj = cnpj.replace(/\D/g, '');
+
+  if (cnpj.length !== 14) {
+      return false;
+  }
+  
+  if (/^(\d)\1+$/.test(cnpj)) {
+      return false;
+  }
+
+  let soma = 0;
+  let peso = 5;
+  for (let i = 0; i < 12; i++) {
+      soma += parseInt(cnpj.charAt(i)) * peso;
+      peso--;
+      if (peso < 2) {
+          peso = 9;
+      }
+  }
+  let digito1 = 11 - (soma % 11);
+  if (digito1 > 9) {
+      digito1 = 0;
+  }
+
+  
+  soma = 0;
+  peso = 6;
+  for (let i = 0; i < 13; i++) {
+      soma += parseInt(cnpj.charAt(i)) * peso;
+      peso--;
+      if (peso < 2) {
+          peso = 9;
+      }
+  }
+  let digito2 = 11 - (soma % 11);
+  if (digito2 > 9) {
+      digito2 = 0;
+  }
+
+  if (parseInt(cnpj.charAt(12)) === digito1 && parseInt(cnpj.charAt(13)) === digito2) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
 $(document).ready(function(){
     $('#cadastrar_seguradora').submit(function(e){
         e.preventDefault();
@@ -13,7 +60,17 @@ $(document).ready(function(){
         var senha = $('[id="senha"]').val();
 
         var senha_hash = hashSenha(senha);
-        
+
+        if (!validarCNPJ(cnpj)) {
+          Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "CNPJ inválido!",
+              heightAuto: false
+          });
+          return;
+      }
+
         var formData = new FormData();
         formData.append('nome', nome);
         formData.append('email', email);
