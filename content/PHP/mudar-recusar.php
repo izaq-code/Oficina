e45eaede30cefbe1732f0328e6ccbe739c6cefcd
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-05-06 00:26:36 -0300

    Update mudar-recusar.php

--- content/PHP/mudar-recusar.php
+++ content/PHP/mudar-recusar.php
@@ -4,7 +4,7 @@ include_once("conexao.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cod = $_POST['codd'];
-    $sql = "UPDATE carro set status_veiculo = 'Aceito' where cod_veiculo = '$cod'";
+    $sql = "UPDATE carro set status_veiculo = 'Recusado' where cod_veiculo = '$cod'";
 
     if ($conexao->query($sql) === TRUE) {
         echo true;
