58a7666f11ceb1059c6a75cb4721b9ab0dbba428
tiagoalcantara06 <162629235+tiagoalcantara06@users.noreply.github.com>
2024-05-05 22:27:54 -0300

    Update perfil-usuario-load.js

--- content/JS/perfil-usuario-load.js
+++ content/JS/perfil-usuario-load.js
@@ -7,7 +7,12 @@ function preencher() {
             contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
             dataType: 'json',
             success: function (data) {
-                console.log(data);
+                console.log(data.resultado[2]);
+                console.log(data.isOf);
+
+
+                data.isOf == true ? exibir2(data) : exibir(data.resultado);
+
                 // alert('descansar né');
                 exibir(data);
             }
@@ -18,14 +23,13 @@ function preencher() {
 function exibir(data) {
 
     // iterando sobre os elementos do array
-    var nome = data[0];
-    var email = data[1];
-    var foto = data[2];
+    var nome = data.resultado[0];
+    var email = data.resultado[1];
+    var foto = data.resultado[2];
 
     //fim da iteração
 
     //enviando os elementos para o html
-    //foto
     var elementos = document.querySelectorAll('.foto1');
 
     // Itera sobre cada elemento
@@ -52,5 +56,44 @@ function exibir(data) {
 
     //email
     $('#email').val(email);
+
+}
+function exibir2(data) {
+    // iterando sobre os elementos do array
+    var nome = data.resultado[0];
+    var email = data.resultado[1];
+    var foto = data.resultado[2];
+
+    //fim da iteração
+
+    //enviando os elementos para o html
+    var elementos = document.querySelectorAll('.foto1');
+
+    // Itera sobre cada elemento
+    elementos.forEach(function (elemento) {
+        // Altera o background do elemento
+        elemento.style.background = 'url(' + foto + ')';
+        elemento.style.backgroundSize = "cover";
+
+    });
+    $('#foto__input9').change(function () {
+        var input = this;
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#previewImage').attr('src', foto).show();
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    //nome
+    t = $('#nome');
+    t.empty();
+    t.append(nome);
+    //email
+    t = $('#email');
+    t.empty();
+    t.append(email);
 
 }
